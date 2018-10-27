16a4c67503b1d095b34e662ef2e532de5bbca0d9
shymaSane <shymaSane1@gmail.com>
2018-10-27 11:59:08 +0200

    fetch weather data using async

--- js/weather.js
+++ js/weather.js
@@ -3,16 +3,15 @@ class Weather{
         this._key = '7eaa66f1b9819a5f2d4f3786cd7abb07';
     }
 
-    getWeather(city, country) {
+    async getWeather(city, country) {
         //fetch information from openweather api
         //units=metric for celsius temp
-        fetch(`http://api.openweathermap.org/data/2.5/weather?APPID=${this._key}&q=${city},${country}&units=metric`)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            return {
-                data
-            }
-        })
+        const fetchWeather = await fetch(`http://api.openweathermap.org/data/2.5/weather?APPID=${this._key}&q=${city},${country}&units=metric`)
+
+        const data = await fetchWeather.json()
+        console.log(data)
+        return {
+            data
+        }
     }
 }
