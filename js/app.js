6d9083082c682c5bc73ba4a2ac6775ba483e9d1b
shymaSane <shymaSane1@gmail.com>
2018-10-28 18:18:32 +0200

    change location and update ui successfuly

--- js/app.js
+++ js/app.js
@@ -1,18 +1,34 @@
-//start app.js when dcument ready
-const weather = new Weather('Amman', 'Jordan')
-const ui = new ModalUi
-document.addEventListener("DOMContentLoaded", function() { 
-   //init weather class with deafult location
 
-//    document.getElementById('card-title').textContent = "amman"
-//    console.log(document.getElementById('card-title').innerHtml)
-   //init ui
-   function getWeather() {
+//init weather class
+const weather = new Weather('Amman', 'Jordan');
+//init ModalUi class
+const ui = new ModalUi
+//start app.js when document ready
+document.addEventListener("DOMContentLoaded", function() { 
+    //add event to the change button in the modal
+    let changeButton  = document.getElementById('change-location')
+    changeButton.addEventListener('click', (e) =>{
+        //without preventDefault the dom wont be manipulated
+        e.preventDefault()
+        //get input values
+        let city = document.getElementById('city').value;
+        let country = document.getElementById('country').value;
+
+        //change the location in weather class the new location inputs
+        weather.changeLocation(city,country);
+        
+        //change weather information
+        changeWeather()
+        
+    })
+
+    //fetch weather data and manipulate the dom
+   function changeWeather() {
        weather.getWeather().then(data => {
            ui.showModal(data)
        })
    }
    
-   getWeather()
+ 
    
 });
--- js/ui.js
+++ js/ui.js
@@ -19,32 +19,5 @@ class ModalUi{
         this.temp_min.textContent = `minmum temp: ${data.locationWeather.main.temp_min}`;
         this.temp_max.textContent = `maximum temp: ${data.locationWeather.main.temp_max}`;
         this.wind.textContent = `Wind: ${data.locationWeather.wind.speed} deg: ${data.locationWeather.wind.deg}`;
-        
-   
-        // console.log(this.main)
-        // this.main.innerHtml = `
-        //     <div class="card text-center col-md-8 col-11 bg-primary text-white">
-        //         <div class="card-header">
-        //             Today's weather
-        //         </div>
-        //         <div class="card-body">
-        //             <h3 class="card-title">${data.locationWeather.name}</h3>
-        //             <p class="card-text">
-        //                 <h5 class="sub-title">${data.locationWeather.main.temp}</h5>
-        //                 <h5 class="subtitl">${data.locationWeather.weather[0].description}</h5>
-        //                 <!-- change when needed -->
-        //                 <span>${data.locationWeather.weather[0].icon}</span>
-        //                 <ul class="list-group list-group-flush">
-        //                     <li class="list-group-item">humidity: ${data.locationWeather.main.humidity}</li>
-        //                     <li class="list-group-item">minmum temp: ${data.locationWeather.main.temp_min}</li>
-        //                     <li class="list-group-item"> maximum temp: ${data.locationWeather.main.temp_max}</li>
-        //                     <li class="list-group-item">Wind: ${data.locationWeather.wind.speed} deg: ${data.locationWeather.wind.deg}</li> 
-        //                 </ul>
-        //             </p>
-        //             <button class="btn btn-primary" data-toggle="modal" data-target="#change-location-modal">Change Location</button>
-        //         </div>
-        //         <div class="card-footer">weather.com</div>
-        //     </div>
-        // `
     }
 }
